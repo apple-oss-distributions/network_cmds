1e9a0ba55006f481e31c2934c192c0515c3d31f0
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2006-02-15 01:39:45 +0000

    network_cmds-245.12
    Imported from network_cmds-245.12.tar.gz

--- racoon.tproj/isakmp_agg.c
+++ racoon.tproj/isakmp_agg.c
@@ -457,18 +457,7 @@ agg_i2recv(iph1, msg)
 	     pa->type != ISAKMP_NPTYPE_NONE;
 	     pa++)
 	{
-		if (pa->type == ISAKMP_NPTYPE_NATD_RFC ||
-			pa->type == ISAKMP_NPTYPE_NATD_DRAFT ||
-			pa->type == ISAKMP_NPTYPE_NATD_BADDRAFT)
-		{
-			if (pa->type != iph1->natd_payload_type) {
-				plog(LLV_ERROR, LOCATION, iph1->remote,
-					"ignore the packet, "
-					"received unexpected natd payload type %d.\n",
-					pa->type);
-				goto end;
-			}			
-			
+		if (pa->type == iph1->natd_payload_type) {
 			natd_match_t match = natd_matches(iph1, pa->ptr);
 			iph1->natt_flags |= natt_natd_received;
 			if ((match & natd_match_local) != 0)
@@ -1388,15 +1377,7 @@ agg_r2recv(iph1, msg0)
 		case ISAKMP_NPTYPE_NATD_DRAFT:
 		case ISAKMP_NPTYPE_NATD_BADDRAFT:
 #ifdef IKE_NAT_T
-			if (pa->type != iph1->natd_payload_type) {
-				plog(LLV_ERROR, LOCATION, iph1->remote,
-					"ignore the packet, "
-					"received unexpected natd payload type %d.\n",
-					pa->type);
-				goto end;
-			}
-				
-			{
+			if (pa->type == iph1->natd_payload_type) {
 				natd_match_t match = natd_matches(iph1, pa->ptr);
 				iph1->natt_flags |= natt_natd_received;
 				if ((match & natd_match_local) != 0)
